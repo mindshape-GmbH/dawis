dc94209d1c4c8a347ed669857441641c778bd70e
Daniel Dorndorf <dorndorf@mindshape.de>
2022-01-04 11:24:52 +0100

    [TASK] Remove Rankalyst module & API

--- service/api/__init__.py
+++ service/api/__init__.py
@@ -1,7 +1,5 @@
-from service.api.rankalyst import Client as RankalystApiClient
 from service.api.sistrix import Client as SistrixApiClient
 
 __all__ = [
-    'RankalystApiClient',
     'SistrixApiClient',
 ]
